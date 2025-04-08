50a42ade3482ae819f524a7d8c051bcbb3965499
Vandana-Saharan <vandanasaharan11@gmail.com>
2025-04-08 12:56:58 +0530

    chart features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,4 +76,3 @@ dependencies {
     // Glide for image loading
     implementation ("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor ("com.github.bumptech.glide:compiler:4.16.0")
-}
